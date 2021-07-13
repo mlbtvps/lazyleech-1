83b34d1551cacc02ccd295e591eb5ce6581667ca
noobsohail <87354331+noobsohail@users.noreply.github.com>
2021-07-13 11:31:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     DB_URL = os.environ.get("DB_URL", "xxxxxxxxxxxx")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", " xxxxx")
     EVERYONE_CHATS = os.environ.get("EVERYONE_CHATS", "-xxxxxxxx")
-    API_ID = int(os.environ.get("API_ID", 4834535))
+    API_ID = int(os.environ.get("API_ID", xxxxx))
     API_HASH = os.environ.get("API_HASH", "xxxxxxxxx")
     LICHER_CHAT = os.environ.get("LICHER_CHAT", "-xxxxxxx")
     ADMIN_CHATS = os.environ.get('ADMIN_CHATS', "-xxxxxx")
